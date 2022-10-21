985c0bce24d475565863f2e807b9dbe72f7eb1a7
Paul Niemczyk <mrpndev@gmail.com>
2022-10-21 08:44:44 -0400

    B

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let btn = document.querySelector("#btn")
+let btn = document.querySelector(".btn")
 
 async function getData() {
     let res = await fetch("https://api.spacexdata.com/v3/capsules")
-    let data = await res.json()
+    let data = res.json()
     
-    renderData(data)
 }
 
 function renderData(data) {
@@ -12,7 +11,7 @@ function renderData(data) {
     
     data.forEach(r => {
         let p = document.createElement("p")
-        p.textContent = `Serial: ${r.capsule_serial} Status: ${r.status} Launched: ${r.original_launch}`
+        p.innerHTML = `Serial: ${r.capsule_serial} Status: ${r.status} Launched: ${r.original_launch}`
         p.className = "ind-rocket"
         let p2 = document.createElement("p")
         p2.textContent = r.details
@@ -24,4 +23,5 @@ function renderData(data) {
 
 btn.addEventListener("click", e => {
     getData()
+    renderData()
 })
